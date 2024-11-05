405d6217143ea6aa96d891d60652ef43f758a40b
Silfur <solargente1508@gmail.com>
2024-11-05 14:06:26 +0100

    on test un truc hein

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+  public static Boolean egalADeux(int n){
+    return n == 2;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
